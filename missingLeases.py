ffd7eea860b4e70c01000d0ba372f0da7d2c6384
ChimeraBlack1 <christopherparke@zoho.com>
2019-11-13 15:13:01 -0500

    script now finds the machines missing from the previous month.  Might make the same script find the ones that were added...

--- missingLeases.py
+++ missingLeases.py
@@ -2,15 +2,14 @@ import math
 import xlrd
 import xlwt
 
-
 #open PREVIOUS month Sherpa Report
-prevReport = "SherpaReport(Oct).xlsm"
-prevwb = xlrd.open_workbook(SherpaReport)
+prevReport = "SherpaReport(Sep).xlsm"
+prevwb = xlrd.open_workbook(prevReport)
 prevSheet = prevwb.sheet_by_index(0)
 
 #open CURRENT month Sherpa Report
-currentReport = "SherpaReport(Sep).xlsm"
-currentwb = xlrd.open_workbook(SherpaReport)
+currentReport = "SherpaReport(Oct).xlsm"
+currentwb = xlrd.open_workbook(currentReport)
 currentSheet = currentwb.sheet_by_index(0)
 
 #write to workbook
@@ -21,6 +20,7 @@ NewWorkbookName = "LeasesLost.xls"
 #Globals
 endOfPrevMonthSheet = 3451
 endOfThisMonthSheet = 3499
+newWorkbookPointer = 0
 #Sherpa
 SherpaReportSerialCol = 10
 SherpaReportAssetPriceCol = 11
@@ -28,50 +28,64 @@ SherpaReportCustomerNameCol = 2
 SherpaCustomerAddyCol = 12
 SherpaReportCustomerModelCol = 9
 SherpaReportTestFunderCol = 6
+newWorkbookPointer = 0
 
-for x in range(1, endOfThisMonthSheet if endOfThisMonthSheet > endOfPrevMonthSheet else endOfPrevMonthSheet):
-  # get serial to test from Sherpa Report
+if(endOfThisMonthSheet < endOfPrevMonthSheet):
+  print("careful, this month has less total assets than last month. Check the code.")
+
+for x in range(1, endOfPrevMonthSheet):
+
+  if endOfThisMonthSheet > endOfPrevMonthSheet:
+    if x > endOfPrevMonthSheet:
+      break
+  elif endOfPrevMonthSheet > endOfThisMonthSheet:
+    if x > endOfThisMonthSheet:
+      break
   found = False
+  # get serial to test from Sherpa Report
   try:
-    testSerial = currentSheet.cell_value(x,SherpaReportSerialCol)
-    testAssetPrice = currentSheet.cell_value(x,SherpaReportAssetPriceCol)
-    testCustomerName = currentSheet.cell_value(x,SherpaReportCustomerNameCol)
-    testCustomerAddy = currentSheet.cell_value(x,SherpaCustomerAddyCol)
-    testCustomerModel = currentSheet.cell_value(x,SherpaReportCustomerModelCol)
-    testFunder = currentSheet.cell_value(x,SherpaReportTestFunderCol)
+    testSerial = prevSheet.cell_value(x,SherpaReportSerialCol)
+    testAssetPrice = prevSheet.cell_value(x,SherpaReportAssetPriceCol)
+    testCustomerName = prevSheet.cell_value(x,SherpaReportCustomerNameCol)
+    testCustomerAddy = prevSheet.cell_value(x,SherpaCustomerAddyCol)
+    testCustomerModel = prevSheet.cell_value(x,SherpaReportCustomerModelCol)
+    testFunder = prevSheet.cell_value(x,SherpaReportTestFunderCol)
     testSerial = int(testSerial)
   except:
     testSerial = str(testSerial)
 
-  # look in the DLL portfolio for the serial
-  for y in range(1, endOfPrevMonthSheet if endOfThisMonthSheet > endOfPrevMonthSheet else endOfThisMonthSheet):
+  # look in the other report for the serial
+  for y in range(1, endOfThisMonthSheet):
     try:
-      otherTestSerial = prevSheet.cell_value(y,SherpaReportSerialCol) if endOfThisMonthSheet > endOfPrevMonthSheet else currentSheet.cell_value(y,SherpaReportSerialCol)
+      otherTestSerial = currentSheet.cell_value(y,SherpaReportSerialCol)
     except:
       continue
 
     if testSerial == "":
-      worksheet.write(x,0,"Blank Serial (DLL)")
-      worksheet.write(x,1, testAssetPrice)
-      worksheet.write(x,2, testCustomerName)
-      worksheet.write(x,3, testCustomerModel)
-      worksheet.write(x,4, testCustomerAddy)
+      worksheet.write(newWorkbookPointer, 0,"Blank Serial")
+      worksheet.write(newWorkbookPointer, 1, testAssetPrice)
+      worksheet.write(newWorkbookPointer, 2, testCustomerName)
+      worksheet.write(newWorkbookPointer, 3, testCustomerModel)
+      worksheet.write(newWorkbookPointer, 4, testCustomerAddy)
+      worksheet.write(newWorkbookPointer, 5, testFunder)
+      newWorkbookPointer = newWorkbookPointer + 1
       continue
     if testSerial == otherTestSerial:
       found = True
       break
  
   #if found, go to next item
-  if found == True: 
+  if found == True:
     continue
   
   if found == False:
-    worksheet.write(x,0, testSerial)
-    worksheet.write(x,1, testAssetPrice)
-    worksheet.write(x,2, testCustomerName)
-    worksheet.write(x,3, testCustomerModel)
-    worksheet.write(x,4, testCustomerAddy)
-    worksheet.write(x,5, testFunder)
+    worksheet.write(newWorkbookPointer, 0, testSerial)
+    worksheet.write(newWorkbookPointer, 1, testAssetPrice)
+    worksheet.write(newWorkbookPointer, 2, testCustomerName)
+    worksheet.write(newWorkbookPointer, 3, testCustomerModel)
+    worksheet.write(newWorkbookPointer, 4, testCustomerAddy)
+    worksheet.write(newWorkbookPointer, 5, testFunder)
+    newWorkbookPointer = newWorkbookPointer + 1 
 
 workbook.save(NewWorkbookName)
 print("saved: " + str(NewWorkbookName))
